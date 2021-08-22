2891e05a78ea0b338257abb655a98add3fc0086e
Richard <richard.yang.tong@gmail.com>
2021-08-22 10:49:28 -0400

    Added notes.

--- 01_HelloWorld/main.cpp
+++ 01_HelloWorld/main.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 
-#define day 7
-
 using namespace std;
 
 int main()
 {
-    cout << "There are " << day << " days in a week!" << endl;
+    cout << "The siezof short = " << sizeof(short) << endl;
+
+    cout << "The siezof int = " << sizeof(int) << endl;
+
+    cout << "The siezof long = " << sizeof(long) << endl;
+
+    cout << "The siezof long long = " << sizeof(long long) << endl;
+
+    cout << "The siezof float = " << sizeof(float) << endl;
+
+    cout << "The siezof double = " << sizeof(double) << endl;
+
+    cout << "The siezof long double = " << sizeof(long double) << endl;
 
     return 0;
 }
