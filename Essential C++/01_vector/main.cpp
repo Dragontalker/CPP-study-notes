447b542558783f17d601d1df5dad86e3656352b4
Richard <richard.yang.tong@gmail.com>
2021-08-22 11:13:16 -0400

    Added notes.

--- Essential C++/01_vector/main.cpp
+++ Essential C++/01_vector/main.cpp
@@ -9,10 +9,19 @@ int main()
 {
     vector<int> pell_sqp(seq_size);
 
-    pell_sqp[0] = 1;
-    pell_sqp[1] = 2;
+    pell_sqp[ 0 ] = 1;
+    pell_sqp[ 1 ] = 2;
 
+    for ( int ix = 2; ix < seq_size; ++ix)
+        pell_sqp[ ix ] = pell_sqp[ ix-2 ] + 2*pell_sqp[ ix - 1];
 
+    cout << "The first " << seq_size
+         << " elements of the Pell Series:\n\t";
+
+    for (int ix = 0; ix < seq_size; ++ix)
+        cout << pell_sqp[ ix ] << ' ';
+
+    cout << '\n';
 
     return 0;
 }
