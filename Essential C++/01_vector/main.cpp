febdf8ca8f038fa57bf81855535905d25c7af04d
Richard <richard.yang.tong@gmail.com>
2021-08-22 11:03:51 -0400

    Added notes.

---
+++ Essential C++/01_vector/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+#define seq_size 18
+
+using namespace std;
+
+int main()
+{
+    vector<int> pell_sqp(seq_size);
+
+    pell_sqp[0] = 1;
+    pell_sqp[1] = 2;
+
+
+
+    return 0;
+}
