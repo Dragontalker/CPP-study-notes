30788a7ca335e8c3d8ff304b619b694c47228f22
Richard <richard.yang.tong@gmail.com>
2021-08-22 10:46:12 -0400

    Added notes.

--- 01_HelloWorld/main.cpp
+++ 01_HelloWorld/main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
+#define day 7
+
 using namespace std;
 
 int main()
 {
-    int a = 10;
-
-    cout << "a = " << a << endl;
+    cout << "There are " << day << " days in a week!" << endl;
 
     return 0;
 }
