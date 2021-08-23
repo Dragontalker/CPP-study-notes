4c823ee33df9f08886d66b4f60e5b15a83e3f5e6
Richard <richard.yang.tong@gmail.com>
2021-08-22 20:55:31 -0400

    Added notes.

--- Essential C++/03_fibonacci/main.cpp
+++ Essential C++/03_fibonacci/main.cpp
@@ -2,8 +2,30 @@
 
 using namespace std;
 
+int fibon_elem(int);
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    int result = fibon_elem(10);
+
+    cout << "The result of 3 = " << result << endl;
+
     return 0;
+}
+
+int fibon_elem(int pos)
+{
+    int elem = 1;
+
+    int n_1 = 1;
+    int n_2 = 1;
+
+    for (int i = 3; i <= pos; i++)
+    {
+        elem = n_2 + n_1;
+        n_2 = n_1;
+        n_1 = elem;
+    }
+
+    return elem;
 }
