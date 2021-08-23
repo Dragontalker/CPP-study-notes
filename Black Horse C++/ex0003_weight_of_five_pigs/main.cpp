554b846556eabefef7e3bbb10c2bd1ba96096221
Richard <richard.yang.tong@gmail.com>
2021-08-23 09:54:54 -0400

    Added notes.

---
+++ Black Horse C++/ex0003_weight_of_five_pigs/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#define N 5
+#define w_1 300
+#define w_2 350
+#define w_3 200
+#define w_4 400
+#define w_5 500
+
+using namespace std;
+
+int main()
+{
+    int arr[N] = {w_1, w_2, w_3, w_4, w_5};
+
+    return 0;
+}
