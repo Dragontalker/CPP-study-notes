707bb0e7b702969c82f585a508028db839e90806
Richard <richard.yang.tong@gmail.com>
2021-08-23 18:57:05 -0400

    Added notes.

--- Black Horse C++/ex0016_struct_as_argument/main.cpp
+++ Black Horse C++/ex0016_struct_as_argument/main.cpp
@@ -10,8 +10,23 @@ struct Student
     double score;
 };
 
+void printStudent(Student);
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    struct Student s1;
+    s1.name = "Tom";
+    s1.age = 18;
+    s1.score = 95.5;
+
+    printStudent(s1);
+
     return 0;
+}
+
+void printStudent(Student student)
+{
+    cout << "Name = " << student.name << endl;
+    cout << "Age = " << student.age << endl;
+    cout << "Score = " << student.score << endl;
 }
