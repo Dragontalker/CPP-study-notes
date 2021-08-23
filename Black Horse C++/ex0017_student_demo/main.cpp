827c46e7300fefeb907b0fd682ce277330e57f85
Richard <richard.yang.tong@gmail.com>
2021-08-23 19:27:16 -0400

    Added notes.

--- Black Horse C++/ex0017_student_demo/main.cpp
+++ Black Horse C++/ex0017_student_demo/main.cpp
@@ -5,19 +5,40 @@ using namespace std;
 
 struct Student
 {
-    string name;
-    int age;
+    string sName;
     double score;
 };
 
 struct Teacher
 {
-    string name;
-    struct Student stuArray[];
+    string tName;
+    struct Student sArray[];
 };
+
+void allocateSpace(struct Teacher[], int);
 
 int main()
 {
 
     return 0;
+}
+
+void allocateSpace(struct Teacher tArray[], int len)
+{
+    string nameSeed = "ABCDE";
+
+    for (int i = 0; i < len; i++)
+    {
+        tArray[i].tName = "Teacher_";
+        tArray[i].tName += nameSeed[i];
+
+        for (int j = 0; i < 5; j++)
+        {
+            tArray[i].sArray[j].sName = "Student_";
+            tArray[i].sArray[j].sName += nameSeed[j];
+
+            tArray[i].sArray[j].score = 60;
+        }
+
+    }
 }
