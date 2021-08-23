4b32164eb680bb86e9b96d39b1996e6072054507
Richard <richard.yang.tong@gmail.com>
2021-08-23 09:56:52 -0400

    Added notes.

--- Black Horse C++/ex0003_weight_of_five_pigs/main.cpp
+++ Black Horse C++/ex0003_weight_of_five_pigs/main.cpp
@@ -5,13 +5,25 @@
 #define w_2 350
 #define w_3 200
 #define w_4 400
-#define w_5 500
+#define w_5 250
 
 using namespace std;
 
 int main()
 {
     int arr[N] = {w_1, w_2, w_3, w_4, w_5};
+
+    int w_max = 0;
+
+    for (int i = 0; i < N; i++)
+    {
+        if (arr[i] > w_max)
+        {
+            w_max = arr[i];
+        }
+    }
+
+    cout << "The maximum weight = " << w_max << "lbs." << endl;
 
     return 0;
 }
