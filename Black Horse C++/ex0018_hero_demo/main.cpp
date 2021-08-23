f76dc8f298260c9c8c73a8ebe8fa76bedf0309e7
Richard <richard.yang.tong@gmail.com>
2021-08-23 19:39:15 -0400

    Added notes.

---
+++ Black Horse C++/ex0018_hero_demo/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+#define N 5
+
+using namespace std;
+
+struct Hero
+{
+    string name;
+    int age;
+    string gender;
+};
+
+int main()
+{
+
+    return 0;
+}
