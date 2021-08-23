86777f0e9b6004cfd2e1ccdbe416c19c8dd24725
Richard <richard.yang.tong@gmail.com>
2021-08-23 09:44:37 -0400

    Added notes.

--- Black Horse C++/ex0001_array/main.cpp
+++ Black Horse C++/ex0001_array/main.cpp
@@ -11,19 +11,22 @@ int main()
     arr[3] = 40;
     arr[4] = 50;
 
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 5; i++)
+    {
         cout << arr[i] << endl;
     }
 
     int arr2[5] = {10, 20, 30, 40, 50};
 
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 5; i++)
+    {
         cout << arr2[i] << endl;
     }
 
     int arr3[] = {90, 80, 70, 60, 50, 40, 30, 20, 10};
 
-    for (int i = 0; i < 9; i++) {
+    for (int i = 0; i < 9; i++)
+    {
         cout << arr3[i] << endl;
     }
 
