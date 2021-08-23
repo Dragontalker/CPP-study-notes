a2dab4cac5f8c2f9d16009ef3a1d043d7fcb49cd
Richard <richard.yang.tong@gmail.com>
2021-08-23 19:01:07 -0400

    Added notes.

--- Black Horse C++/ex0016_struct_as_argument/main.cpp
+++ Black Horse C++/ex0016_struct_as_argument/main.cpp
@@ -10,7 +10,8 @@ struct Student
     double score;
 };
 
-void printStudent(struct Student);
+void printStudent1(struct Student);
+void printStudent2(struct Student*);
 
 int main()
 {
@@ -19,14 +20,23 @@ int main()
     s1.age = 18;
     s1.score = 95.5;
 
-    printStudent(s1);
+    printStudent1(s1);
+
+    printStudent2(&s1);
 
     return 0;
 }
 
-void printStudent(struct Student student)
+void printStudent1(struct Student student)
 {
     cout << "Name = " << student.name << endl;
     cout << "Age = " << student.age << endl;
     cout << "Score = " << student.score << endl;
+}
+
+void printStudent2(struct Student* ptr)
+{
+    cout << "Name = " << ptr->name << endl;
+    cout << "Age = " << ptr->age << endl;
+    cout << "Score = " << ptr->score << endl;
 }
