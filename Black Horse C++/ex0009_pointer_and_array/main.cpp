e7d555db0da92bd7212a71b08887c04ba7126f79
Richard <richard.yang.tong@gmail.com>
2021-08-23 12:22:26 -0400

    Added notes.

--- Black Horse C++/ex0009_pointer_and_array/main.cpp
+++ Black Horse C++/ex0009_pointer_and_array/main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 
+#define N 10
+
 using namespace std;
 
 int main()
 {
-    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int arr[N] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
     int* ptr = arr;
 
     cout << "The first element in arr = " << *(ptr) << endl;
+    cout << "The second element in arr = " << *(ptr + 1) << endl;
+    cout << "The last element in arr = " << *(ptr + N - 1) << endl;
 
 
     return 0;
