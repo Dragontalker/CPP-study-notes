f7ed2c373cdfd11947854acfc5f8aa7c80ef1e6c
Richard <richard.yang.tong@gmail.com>
2021-08-23 16:35:15 -0400

    Added notes.

---
+++ Black Horse C++/ex0015_nested_struct/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Student
+{
+    string name;
+    int age;
+    double score;
+};
+
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
