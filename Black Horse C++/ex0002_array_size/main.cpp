67b3ec75cd4cb29b85cec4dc7dddd14f8f8c12dc
Richard <richard.yang.tong@gmail.com>
2021-08-23 09:48:13 -0400

    Added notes.

--- Black Horse C++/ex0002_array_size/main.cpp
+++ Black Horse C++/ex0002_array_size/main.cpp
@@ -6,7 +6,11 @@ int main()
 {
     int arr[5] = {1, 2, 3, 4, 5};
 
-    cout << "The size of arr = " << sizeof(arr) << endl;
+    cout << "The size of arr = " << sizeof(arr)
+         << " bytes." << endl;
+
+    cout << "The size of arr[0] = " <<  sizeof(arr[0])
+         << " bytes." << endl;
 
     return 0;
 }
