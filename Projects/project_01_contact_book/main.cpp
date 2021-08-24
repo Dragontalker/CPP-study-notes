e73c2730730efc448b335f34736736f9a6711064
Richard <richard.yang.tong@gmail.com>
2021-08-24 09:34:16 -0400

    Added notes.

--- Projects/project_01_contact_book/main.cpp
+++ Projects/project_01_contact_book/main.cpp
@@ -2,8 +2,27 @@
 
 using namespace std;
 
+void showMenu();
+
 int main()
 {
-    cout << "Hello world!" << endl;
+
+    showMenu();
+
+    system("pause");
+
     return 0;
 }
+
+void showMenu()
+{
+    cout << "1. Add contact" << endl;
+    cout << "2. Display contact" << endl;
+    cout << "3. Delete contact" << endl;
+    cout << "4. Search contact" << endl;
+    cout << "5. Edit contact" << endl;
+    cout << "6. Clear contact" << endl;
+    cout << "0. Exit contact book" << endl;
+}
+
+
