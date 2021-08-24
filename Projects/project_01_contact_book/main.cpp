2009ad389faddf06256b8046fe5529e0f1eeb022
Richard <richard.yang.tong@gmail.com>
2021-08-24 09:36:26 -0400

    Added notes.

--- Projects/project_01_contact_book/main.cpp
+++ Projects/project_01_contact_book/main.cpp
@@ -16,13 +16,15 @@ int main()
 
 void showMenu()
 {
-    cout << "1. Add contact" << endl;
-    cout << "2. Display contact" << endl;
-    cout << "3. Delete contact" << endl;
-    cout << "4. Search contact" << endl;
-    cout << "5. Edit contact" << endl;
-    cout << "6. Clear contact" << endl;
-    cout << "0. Exit contact book" << endl;
+    cout << "******************************" << endl;
+    cout << "***** 1. Add contact *********" << endl;
+    cout << "***** 2. Display contact *****" << endl;
+    cout << "***** 3. Delete contact ******" << endl;
+    cout << "***** 4. Search contact ******" << endl;
+    cout << "***** 5. Edit contact ********" << endl;
+    cout << "***** 6. Clear contact *******" << endl;
+    cout << "***** 0. Exit contact book****" << endl;
+    cout << "******************************" << endl;
 }
 
 
