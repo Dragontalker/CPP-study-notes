2a796528e5b031ac3a70fab178f42915e782f915
Richard <richard.yang.tong@gmail.com>
2021-08-25 13:37:35 -0400

    Added notes.

--- Black Horse C++/ex0022_new/main.cpp
+++ Black Horse C++/ex0022_new/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 
 int* func(void);
+void test01(void);
 
 int main()
 {
-
+    test01();
 
     return 0;
 }
@@ -13,4 +14,18 @@ int* func()
 {
     int* p = new int(10);
     return p;
+}
+
+void test01()
+{
+    int* p = func();
+
+    std::cout << *(p) << std::endl;
+    std::cout << *(p) << std::endl;
+    std::cout << *(p) << std::endl;
+    std::cout << *(p) << std::endl;
+
+    delete(p);
+
+    std::cout << *(p) << std::endl;
 }
