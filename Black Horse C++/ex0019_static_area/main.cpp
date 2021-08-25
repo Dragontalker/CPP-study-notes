b19531d2aafdb71dcf1006fb9709b18a43688181
Richard <richard.yang.tong@gmail.com>
2021-08-25 11:33:05 -0400

    Added notes.

--- Black Horse C++/ex0019_static_area/main.cpp
+++ Black Horse C++/ex0019_static_area/main.cpp
@@ -20,5 +20,10 @@ int main()
     std::cout << "The address of local variable g_b = "
               << (int)&g_b << std::endl;
 
+    static int s_a = 10;
+
+    std::cout << "The address of local variable s_a = "
+              << (int)&s_a << std::endl;
+
     return 0;
 }
