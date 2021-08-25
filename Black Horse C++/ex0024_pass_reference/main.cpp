d344154d2a2fdf0658a0724a84ca774b87e8173d
Richard <richard.yang.tong@gmail.com>
2021-08-25 14:06:31 -0400

    Added notes.

--- Black Horse C++/ex0024_pass_reference/main.cpp
+++ Black Horse C++/ex0024_pass_reference/main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 
+void mySwap01(int, int);
+
 int main()
 {
+    int a = 10;
+    int b = 20;
+
+    mySwap01(a, b);
+
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
 
     return 0;
+}
+
+void mySwap01(int a, int b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+
+    std::cout << "In mySwap01, a = " << a << std::endl;
+    std::cout << "In mySwap01, b = " << b << std::endl;
 }
