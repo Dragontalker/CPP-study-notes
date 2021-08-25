09b1c8882d6439e4d196c88120388e9bcf8028c5
Richard <richard.yang.tong@gmail.com>
2021-08-25 13:26:59 -0400

    Added notes.

--- Black Horse C++/ex0021_heap/main.cpp
+++ Black Horse C++/ex0021_heap/main.cpp
@@ -7,9 +7,15 @@ int main()
     int* p = func();
 
     std::cout << "The value pointed by p = " << *(p) << std::endl;
+    std::cout << "The value pointed by p = " << *(p) << std::endl;
+    std::cout << "The value pointed by p = " << *(p) << std::endl;
+    std::cout << "The value pointed by p = " << *(p) << std::endl;
 
     free(p);
 
+    std::cout << "The value pointed by p = " << *(p) << std::endl;
+    std::cout << "The value pointed by p = " << *(p) << std::endl;
+    std::cout << "The value pointed by p = " << *(p) << std::endl;
     std::cout << "The value pointed by p = " << *(p) << std::endl;
 
     return 0;
