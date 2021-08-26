3e37643d838d2f8f995933f8cabbe7d1f2709371
Richard <richard.yang.tong@gmail.com>
2021-08-25 21:43:22 -0400

    Added notes.

--- Black Horse C++/ex0027_encapsulation/main.cpp
+++ Black Horse C++/ex0027_encapsulation/main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 
+#define PI 3.1415926
+
+class Circle
+{
+public:
+
+    double m_r;
+
+    double calculateZC()
+    {
+        return 2 * PI * m_r;
+    }
+};
+
 int main()
 {
+    Circle c1;
+    c1.m_r = 10;
+
+    std::cout << "The raius of our circle = "
+              << c1.calculateZC() << std::endl;
 
     return 0;
 }
