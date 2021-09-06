0a329f874d0d97ad3253834cc5f086553d5b2eef
Richard <richard.yang.tong@gmail.com>
2021-09-06 12:03:24 -0400

    Added notes.

---
+++ Thinking in C++/p111_ex01/main.cpp
@@ -0,0 +1,9 @@
+//: C02:Incident.cpp
+// Records an incident using INCR
+#include <cstdlib> // Declare "system()"
+using namespace std;
+
+int main() {
+    // Other code here...
+    system("incr < incident.dat > incident.dat");
+} ///:~
