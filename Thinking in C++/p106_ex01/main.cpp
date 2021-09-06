9153673c57a9b93770ceb3dfb111bd2aeeb264c4
Richard <richard.yang.tong@gmail.com>
2021-09-06 11:43:10 -0400

    Added notes.

---
+++ Thinking in C++/p106_ex01/main.cpp
@@ -0,0 +1,9 @@
+//: C02:Hello.cpp
+// Saying Hello with C++
+#include <iostream> // Stream declarations
+using namespace std;
+
+int main() {
+    cout << "Hello world! I am "
+         << 8 << " Today!" << endl;
+} ///:~
