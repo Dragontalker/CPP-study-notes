dfa9f59f0d41d8ab8e146c69ed5decac70ecb4d4
Richard <richard.yang.tong@gmail.com>
2021-09-06 18:23:32 -0400

    Added notes.

---
+++ Thinking in C++/p182_ex01/main.cpp
@@ -0,0 +1,13 @@
+//: C03:const_cast.cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int i = 0;
+    int* j = (int*)&i; // Deprecated form
+    j = const_cast<int*>(&i); // Preferred
+    // Can't do simultaneous additional casting:
+    //! long* l = const_cast<long*>(&i); // Error
+    volatile int k = 0;
+    int* u = const_cast<int*>(&k);
+} ///:~
