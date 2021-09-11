f5c1e8c3f828243bd74f5f3d1bb2fafcfbadaa96
Richard <richard.yang.tong@gmail.com>
2021-09-11 10:20:43 -0400

    Added notes.

--- Thinking in C++/p229_ex01/CLib.cpp
+++ Thinking in C++/p229_ex01/CLib.cpp
@@ -2,7 +2,9 @@
 // Implementation of example C-like library
 // Declare structure and functions;
 #include "CLib.h"
+#include <fstream>
 #include <iostream>
+#include <string>
 #include <cassert>
 using namespace std;
 
@@ -59,8 +61,33 @@ void cleanup(CStash* s) {
     }
 } ///:~
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
-}
+int main() {
+    // Define variables at the beginning
+    // of the block, as in C:
+    CStash intStash, stringStash;
+    int i;
+    char* cp;
+    ifstream in;
+    string line;
+    const int bufsize = 80;
+    // Now remember to initialize the variables:
+    initialize(&intStash, sizeof(int));
+    for(i = 0; i < 100; i++)
+        add(&intStash, &i);
+    for(i = 0; i < count(&intStash); i++)
+        cout << "fetch(&intStash, " << i << ") = "
+             << *(int*)fetch(&intStash, i)
+             << endl;
+    // Holds 80-character strings:
+    initialize(&stringStash, sizeof(char)*bufsize);
+    in.open("CLibTest.cpp");
+    assert(in);
+    while(getline(in, line))
+        add(&stringStash, line.c_str());
+    i = 0;
+    while((cp = (char*)fetch(&stringStash, i++)) != 0)
+        cout << "fetch(&stringStash, " << i << ") = "
+             << cp << endl;
+    cleanup(&intStash);
+    cleanup(&stringStash);
+} ///:~
