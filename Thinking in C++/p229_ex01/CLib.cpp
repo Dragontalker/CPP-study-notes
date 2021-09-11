ede81a385e8f59ca7d5c43cd8d9b339ceb3516ce
Richard <richard.yang.tong@gmail.com>
2021-09-11 10:00:23 -0400

    Added notes.

--- Thinking in C++/p229_ex01/CLib.cpp
+++ Thinking in C++/p229_ex01/CLib.cpp
@@ -17,6 +17,19 @@ void initialize(CStash* s, int sz) {
     s->next = 0;
 }
 
+int add(CStash* s, const void* element) {
+    if(s->next >= s->quantity) //Enough space left?
+        inflate(s, increment);
+    // Copy element into storage;
+    // starting at next empty space;
+    int startBytes = s->next * s->size;
+    unsigned char* e = (unsigned char*)element;
+    for(int i = 0; i < s->size; i++)
+        s->storage[startBytes + i] = e[i];
+    s->next++;
+    return (s->next - 1); // Index number
+}
+
 int main()
 {
     cout << "Hello world!" << endl;
