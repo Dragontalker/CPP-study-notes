74e9565b60c3d78f9ea60d57d873a8fb01cc7c97
Richard <richard.yang.tong@gmail.com>
2021-09-14 11:13:03 -0400

    Added notes.

--- Thinking in C++/p250_ex01/Stack.cpp
+++ Thinking in C++/p250_ex01/Stack.cpp
@@ -4,32 +4,27 @@
 #include ""../require.h"
 using namespace std;
 
-void
-Stack::Link::initialize(void* dat, Link* nxt) {
+void Stack::Link::initialize(void* dat, Link* nxt) {
     data = dat;
     next = nxt;
 }
 
-void
-Stack::initialize() {
+void Stack::initialize() {
     head = 0;
 }
 
-void
-Stack::push(void* dat) {
+void Stack::push(void* dat) {
     Link* newLink = new Link;
     newLink->initialize(dat, head);
     head = newLink;
 }
 
-void*
-Stack::peek() {
+void* Stack::peek() {
     require(head != 0, "Stack empty");
     return head->data;
 }
 
-void*
-Stack::pop() {
+void* Stack::pop() {
     if(head == 0) return 0;
     void* result = head->data;
     Link* oldHead = head;
@@ -38,7 +33,6 @@ Stack::pop() {
     return result;
 }
 
-void
-Stack::cleanup() {
+void Stack::cleanup() {
     require(head == 0, "Stack not empty");
 }
