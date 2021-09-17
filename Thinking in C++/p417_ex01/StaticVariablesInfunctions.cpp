cb1d86a3b3297059aabeaab90948c0eb080ca88d
Richard <richard.yang.tong@gmail.com>
2021-09-17 11:33:54 -0400

    Added notes.

--- Thinking in C++/p417_ex01/StaticVariablesInfunctions.cpp
+++ Thinking in C++/p417_ex01/StaticVariablesInfunctions.cpp
@@ -3,8 +3,27 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+char oneChar(const char* charArray = 0) {
+    static const char* s;
+
+    if(charArray) {
+        s = charArray;
+        return *s;
+    } else {
+        require(s, "un-intialized s");
+    }
+
+    if(*s == '\0')
+        return 0;
+    return *s++;
+}
+
+char* a = "abcdefghijklmnopqrstuvwxyz";
+
+int main() {
+    // oneChar(); // require() fails
+    oneChar(a); // Initializes s to a
+    char c;
+    while((c = oneChar()) != 0)
+        cout << c << endl;
 }
