a71dee6c40e1f665b6cb5b33014b0f90dd9bfba8
Richard <richard.yang.tong@gmail.com>
2021-09-17 10:36:01 -0400

    Added notes.

---
+++ Thinking in C++/p000_require/require.h
@@ -0,0 +1,12 @@
+//: :require.h
+// Test for error conditions in programs
+// Local "using namespace std" for old compilers
+#ifndef REQUIRE_H
+#define REQUIRE_H
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+
+
+#endif // REQUIRE_H
