c61722f52d4c204fa7ca156ab8b8e349d504c85c
Richard <richard.yang.tong@gmail.com>
2021-09-17 16:48:58 -0400

    Added notes.

---
+++ Thinking in C++/p428_ex01/NamespaceMath.h
@@ -0,0 +1,19 @@
+//: C10:NamespaceMath.h
+#ifndef NAMESPACEMATH_H
+#define MAMESPACEMATH_H
+namespace Math {
+    enum sign { positive, negative };
+    class Integer {
+        int i;
+        sign s;
+    public:
+        Intger(int ii = 0): i(ii), s(i >= 0 ? positive : negative) {}
+        sign getSign() const { return s; }
+        void setSign(sign sgn) { s = sgn; }
+        // ...
+    };
+    Integer a, b, c;
+    Integer divide(Integer, Integer);
+    // ...
+}
+#endif // NAMESPACEMAT_H
