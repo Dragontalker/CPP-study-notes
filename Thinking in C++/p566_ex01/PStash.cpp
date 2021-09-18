0d90f1cf9ad44c6300788a0a0fd1cde3acf7fef9
Richard <richard.yang.tong@gmail.com>
2021-09-18 12:47:32 -0400

    Added notes.

--- Thinking in C++/p566_ex01/PStash.cpp
+++ Thinking in C++/p566_ex01/PStash.cpp
@@ -13,3 +13,36 @@ int PStash::add(void* element) {
     storage[next++] = element;
     return(next - 1); // Index number
 }
+
+// No ownership:
+PStash::~PStash() {
+    for(int i = 0; i < next; i++)
+        require(storage[i] == 0, "PStash not cleaned up");
+    delete []storage;
+}
+
+// Operator overloading replacement for fetch
+void* PStash::operator[](int index) const {
+    require(index >= 0, "PStash::operator[] index negative");
+    if(index >= next)
+        return 0; // To indicate the end
+    // Produce pointer to desired element:
+    return storage[index];
+}
+
+void* PStash::remove(int index) {
+    void* v = operator[](index);
+    // "Remove" the pointer:
+    if(v != 0) storage[index] = 0;
+    return v;
+}
+
+void PStash::inflate(int increase) {
+    const int psz = sizeof(void*);
+    void** st = new void*[quantity + increase];
+    memset(st, 0, (quantity + increase) * psz);
+    memcpy(st, storage, quantity * psz);
+    quantity += increase;
+    delete []storage; // Old storage
+    storage = st; // Point to new memory
+}
