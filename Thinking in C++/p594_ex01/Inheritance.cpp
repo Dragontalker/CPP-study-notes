64d7be34d4b58cfe345eed0965bb8e4f705fd2e2
Richard <richard.yang.tong@gmail.com>
2021-09-18 16:13:14 -0400

    Added notes.

--- Thinking in C++/p594_ex01/Inheritance.cpp
+++ Thinking in C++/p594_ex01/Inheritance.cpp
@@ -1,9 +1,31 @@
+//: C14:Inheritance.cpp
+// Simple inheritance
+#include "Useful.h"
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+class Y : public X {
+    int i; // Different from X's i
+public:
+    Y() { i = 0; }
+    int change() {
+        i = permute(); // Different name call
+        return i;
+    }
+    void set(int ii) {
+        i = ii;
+        X::set(ii); // Same-name function call
+    }
+};
+
+int main() {
+    cout << "sizeof(X) = " << sizeof(X) << endl;
+    cout << "sizeof(Y) = " << sizeof(Y) << endl;
+    Y D;
+    D.change();
+    // X function interface comes through:
+    D.read();
+    D.permute();
+    // Redefined functios hide base versions:
+    D.set(12);
 }
